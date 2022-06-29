492ebea349a84b83f415622dcba3fafb409ea330
Miezhiko <Miezhiko@gmail.com>
2022-06-29 11:34:56 +0400

    linking
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- config.mk
+++ config.mk
@@ -6,8 +6,8 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+INCS = -I. -I${X11INC}
+LIBS = -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
